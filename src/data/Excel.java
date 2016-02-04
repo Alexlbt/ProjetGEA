ed57f1fd9dac52425565b314bc5c92bb8c6704df
Slogi <slogi@live.com>
2016-02-04 12:18:21 +0100

    TestExcel

---
+++ src/data/Excel.java
@@ -0,0 +1,43 @@
+package data;
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+
+public class Excel {
+
+		public static void main(String[] args) {
+			Workbook workbook = null;
+			try {
+				/* Récupération du classeur Excel (en lecture) */
+				workbook = Workbook.getWorkbook(new File("exemple.xlsx"));
+				
+				/* Un fichier excel est composé de plusieurs feuilles, on y accède de la manière suivante*/
+				Sheet sheet = workbook.getSheet(0);
+				
+				/* On accède aux cellules avec la méthode getCell(indiceColonne, indiceLigne) */
+				Cell a1 = sheet.getCell(0,0);
+				
+				/* On peut récupérer le contenu d'une cellule en utilisant la méthode getContents() */
+				String contenuA1= a1.getContents();
+				
+				System.out.println(contenuA1);
+			} 
+			catch (BiffException e) {
+				e.printStackTrace();
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			} 
+			finally {
+				if(workbook!=null){
+					/* On ferme le worbook pour libérer la mémoire */
+					workbook.close(); 
+				}
+			}
+		}
+	}
